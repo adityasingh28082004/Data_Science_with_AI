14cb785d8701e9ce300f2fc42a153048843ff003
adityasingh28082004 <singhadityaraj968@gmail.com>
2026-09-24 19:50:17 +0530

    77

---
+++ python/chapter1/poem.py
@@ -0,0 +1,6 @@
+print("""winkle, twinkle, little star,
+    #   jfkfkfkf
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+""")
---
+++ python/chapter2/problem2.py
@@ -0,0 +1,4 @@
+a=87
+
+b=7
+print("what is remainder when a is divided b ", a%b)
